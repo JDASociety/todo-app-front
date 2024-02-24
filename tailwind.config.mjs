84f013cbfdbc373811326a2e7f2a6388ede7a908
diegosiac <diego35502@hotmail.com>
2024-02-24 12:24:55 -0600

    Add custom colors and box shadow to tailwind.config.js

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,18 @@ export default {
 		extend: {
 			backgroundImage: {
         'colors': "url('/bg-colors.webp')",
-      }
+      },
+			textColor: {
+				'secondary': '#bfbdb6', 
+			},
+			backgroundColor: {
+				'primary': '#121112',
+				'secondary': '#1a191a',
+				'tertiary': '#bfbdb6',	
+			},
+			boxShadowColor: {
+				primary: '#222122',	
+			}	
 		},
 	},
 	plugins: [],
