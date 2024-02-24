72bd207459d525fc7a89b99cb52b1797d405da33
diegosiac <diego35502@hotmail.com>
2024-02-23 20:07:43 -0600

    Refactor Todo interface and add CreateTodo type

--- src/interfaces/todo.interface.ts
+++ src/interfaces/todo.interface.ts
@@ -1,11 +1,10 @@
 
 export interface Todo {
-
-    title: string;
-    description: string
-    done: boolean
-    created_at: string
-    updated_at: string
-
+    title:       string;
+    description: string;
+    done:        boolean;
+    created_at:  string;
+    updated_at:  string;
 }
 
+export type CreateTodo = Omit<Todo, 'created_at' | 'updated_at'>;
