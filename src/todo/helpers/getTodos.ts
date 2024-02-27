b2a32df3bb962a03147054ba585bc06f97100141
diegosiac <diego35502@hotmail.com>
2024-02-26 21:55:42 -0600

    Refactor getTodos function to include additional query parameters

--- src/todo/helpers/getTodos.ts
+++ src/todo/helpers/getTodos.ts
@@ -1,13 +1,15 @@
 import { getEnvironments } from "../../helpers";
-import type { Todo } from "../";
+import type { ResponseTodoList, Todo } from "../";
 
 const { API_URL } = getEnvironments()
 
-interface Props {
+export interface GetTodosProps {
   page?: number;
   take?: number;
   isDone?: boolean;
   token: string;
+  startDate?: string | Date;
+  endDate?: string | Date;
 }
 
 interface Response {
@@ -16,32 +18,47 @@ interface Response {
   todos: Todo[];
 }
 
-export const getTodos = async({ page = 1, take = 8, isDone, token }: Props): Promise<Response> => {
+export const getTodos = async({ token, page = 1, take = 5, isDone, endDate, startDate }: GetTodosProps): Promise<Response> => {
   if (isNaN(Number(page))) page = 1;
   if (page < 1) page = 1;
 
-  try {
+  if (isNaN(Number(take))) take = 5;
+  if (take < 1) take = 5;
 
-    const response = await fetch(`${API_URL}/todo/`, {
+
+  let query = `page_size=${take}&page=${page}&`
+
+  try {
+    if (isDone !== undefined) {
+      query += `done=${isDone}&`;
+    }
+
+    if (startDate !== undefined && endDate !== undefined) {
+      query += `start_date=${startDate}&end_date=${endDate}&`;
+    }
+    console.log(query)
+
+    const response = await fetch(`${API_URL}/todo/?${query}`, {
       method: 'GET',
       headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${token}`
-      }
+      },
+      cache: 'no-cache'
     });
 
     if (!response.ok) {
       throw new Error('Hubo un error al obtener las tareas.')
     }
 
-    const todos = await response.json();
+    const { count, next, previous, results } = await response.json() as ResponseTodoList;
 
-    const totalPages = 0;
+    const totalPages = Math.ceil(count / take);
 
     return {
       currentPage: page,
       totalPages,
-      todos
+      todos: results
     }
   } catch (error) {
     throw new Error('Hubo un error al obtener las tareas.')
